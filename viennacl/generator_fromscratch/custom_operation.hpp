fe4897097da793341f55e6ba41dad2416d6bcfb9
Philippe Tillet <Phil.Tillet@gmail.com>
2012-12-05 03:16:44 +0800

    Added extract_if

--- viennacl/generator_fromscratch/custom_operation.hpp
+++ viennacl/generator_fromscratch/custom_operation.hpp
@@ -3,6 +3,7 @@
 
 
 #include "viennacl/generator_fromscratch/symbolic_types.hpp"
+#include "viennacl/generator_fromscratch/tree_utils.hpp"
 
 namespace viennacl
 {
@@ -11,6 +12,10 @@ namespace viennacl
   /** @brief A class for making a custom operation */
       class custom_operation
       {
+
+          static bool test_pred(infos_base* p){
+              return dynamic_cast<vec_infos_base *>(p);
+          }
 
         public :
 
@@ -22,7 +27,7 @@ namespace viennacl
           custom_operation ( T0 const & , std::string const & operation_name) : program_name_(operation_name)
           {
               infos_base & tree = T0::get();
-              std::cout << tree.name() << std::endl;
+              std::tree_utils::extract_if(&tree,test_pred);
           }
 
         private:
