95492bc4978dc6970fefe45b6e3f714243dc1d18
Philippe Tillet <Phil.Tillet@gmail.com>
2012-12-05 21:26:34 +0800

    Started body generation

--- viennacl/generator_fromscratch/code_generation/frontend.hpp
+++ viennacl/generator_fromscratch/code_generation/frontend.hpp
@@ -1,9 +1,13 @@
-#ifndef VIENNACL_GENERATOR_KERNEL_GENERATOR_FRONTEND_HPP
-#define VIENNACL_GENERATOR_KERNEL_GENERATOR_FRONTEND_HPP
+#ifndef VIENNACL_GENERATOR_CODE_GENERATION_FRONTEND_HPP
+#define VIENNACL_GENERATOR_CODE_GENERATION_FRONTEND_HPP
 
 #include "viennacl/generator_fromscratch/symbolic_types.hpp"
 #include "viennacl/generator_fromscratch/tree_utils.hpp"
+#include "viennacl/generator_fromscratch/code_generation/backend.hpp"
+#include "viennacl/generator_fromscratch/code_generation/utils.hpp"
+
 #include <algorithm>
+
 namespace viennacl{
 
     namespace generator{
@@ -17,13 +21,10 @@ namespace viennacl{
                     return lhs->id() < rhs->id();
                 }
 
-            public:
-                frontend(std::list<infos_base*> const & trees, std::string const & op_name) : trees_(trees), op_name_(op_name){ }
-
                 std::string generate_headers() const{
                     std::string res;
                     res+="__kernel void " + op_name_ + "(";
-                    std::list<kernel_argument*> args(tree_utils::extract_type<kernel_argument>(trees_));
+                    std::list<kernel_argument*> args(utils::cast<kernel_argument>(utils::extract_if(trees_,code_generation::utils::is_type<kernel_argument>)));
                     args.sort(sort_pred);
                     for(std::list<kernel_argument*>::iterator it = args.begin() ; it!= args.end() ; ++it){
                         if(it!=args.begin()) res+=",";
@@ -35,8 +36,20 @@ namespace viennacl{
 
                 std::string generate_sources() const{
                     std::string res;
-                    return res;
+                    std::list<infos_base *> vec_exprs(code_generation::utils::extract_if (trees_,code_generation::utils::is_type<vector_expression_infos_base>));
+                    std::list<infos_base *> scal_exprs(code_generation::utils::extract_if(trees_,code_generation::utils::is_type<scalar_expression_infos_base>));
+                    std::list<infos_base *> mat_exprs(code_generation::utils::extract_if (trees_,code_generation::utils::is_type<matrix_expression_infos_base>));
+                    code_generation::blas1_generator gen(vec_exprs,scal_exprs);
+                    return gen();
                 }
+
+            public:
+                frontend(std::list<infos_base*> const & trees, std::string const & op_name) : trees_(trees), op_name_(op_name){ }
+
+                std::string generate() const{
+                    return generate_headers() + generate_sources();
+                }
+
 
             private:
                 std::list<infos_base*> trees_;
--- viennacl/generator_fromscratch/code_generation/utils.hpp
+++ viennacl/generator_fromscratch/code_generation/utils.hpp
@@ -1,6 +1,9 @@
 #ifndef VIENNACL_GENERATOR_CODE_GENERATION_UTILS_HPP
 #define VIENNACL_GENERATOR_CODE_GENERATION_UTILS_HPP
 
+#include <list>
+#include <set>
+
 namespace viennacl{
 
     namespace generator{
@@ -8,8 +11,89 @@ namespace viennacl{
 
         namespace code_generation{
 
+
             namespace utils{
 
+
+                template<class T>
+                static bool is_type(infos_base* p){
+                    return dynamic_cast<T *>(p);
+                }
+
+
+                template<class Pred>
+                static std::list<infos_base*> extract_if(infos_base* const node, Pred pred, bool inspect_binary_leaves=true){
+                    std::list<infos_base*> res;
+                    if(arithmetic_tree_infos_base * p = dynamic_cast<arithmetic_tree_infos_base *>(node)){
+                            std::list<infos_base*> tmplhs(extract_if(&p->lhs(),pred));
+                            std::list<infos_base*> tmprhs(extract_if(&p->rhs(),pred));
+                            res.merge(tmplhs);
+                            res.merge(tmprhs);
+                    }
+                    else if(unary_tree_infos_base * p = dynamic_cast<unary_tree_infos_base *>(node)){
+                        std::list<infos_base*> tmp(extract_if(&p->sub(),pred));
+                        res.merge(tmp);
+                    }
+                    if(pred(node)){
+                        res.push_back(node);
+                    }
+                    return res;
+                }
+
+                template<class Pred>
+                static std::list<infos_base*> extract_if(std::list<infos_base*> const & trees, Pred pred, bool inspect_binary_leaves=true){
+                    std::list<infos_base*> res;
+                    for(std::list<infos_base*>::const_iterator it = trees.begin() ; it != trees.end() ; ++it){
+                        std::list<infos_base*> tmp(extract_if(*it,pred));
+                        res.merge(tmp);
+                    }
+                    return res;
+                }
+
+
+                template<class T, class B>
+                static std::list<T *> cast(std::list<B *> const & in){
+                    std::list<T*> res(in.size());
+                    std::transform(in.begin(), in.end(), res.begin(),Base2Target<B,T>());
+                    return res;
+                }
+
+                template<class T>
+                static std::list<T *> extract_cast(std::list<infos_base*> const & trees){
+                    return cast<T>(extract_if(trees,is_type<T>));
+                }
+
+                template<class T>
+                class cache_manager{
+                public:
+                    cache_manager(std::list<T * > const & expressions,  std::ostringstream & oss, std::set<T *>& cached_entries) : expressions_(expressions)
+                                                                                                                                               , oss_(oss)
+                      ,cached_entries_(cached_entries){         }
+
+                    void fetch_entries(std::string const & idx){
+                        for(typename std::list<T * >::iterator it = expressions_.begin() ; it != expressions_.end() ; ++it){
+                            T * p = *it;
+                            if(cached_entries_.insert(p).second){
+                                p->access_name(p->name()+"_val");
+                                oss_ << p->scalartype() << " " << p->generate() << " = " << p->name() << "[" << idx << "];\n";
+                            }
+                        }
+                    }
+
+                    void writeback_entries(std::string const & idx){
+                        for(typename std::list<T * >::iterator it = expressions_.begin() ; it != expressions_.end() ; ++it){
+                            T * p = *it;
+                            if(p->is_modified())
+                                oss_<< p->name() << "[" << idx << "]"<< " = "  << p->generate() << ";\n";
+                        }
+                    }
+
+                private:
+                    std::list<T * > expressions_;
+                    std::ostringstream & oss_;
+                    std::set<T *> & cached_entries_;
+
+                };
 
             }
 
