73ec30c86357307e71f7ecb3ed23099f856045d7
Philippe Tillet <Phil.Tillet@gmail.com>
2012-12-11 19:37:09 +0800

    Replaced reference by smart pointer for operators

---
+++ viennacl/generator_fromscratch/forwards.h
@@ -0,0 +1,24 @@
+#ifndef VIENNACL_GENERATOR_FORWARDS_H
+#define VIENNACL_GENERATOR_FORWARDS_H
+
+namespace viennacl{
+
+namespace generator{
+
+class infos_base;
+class assign_type;
+class add_type;
+class inplace_add_type;
+class sub_type;
+class inplace_sub_type;
+class scal_mul_type;
+class inplace_scal_mul_type;
+class scal_div_type;
+class inplace_scal_div_type;
+class elementwise_prod_type;
+class elementwise_div_type;
+
+}
+
+}
+#endif // FORWARDS_H
