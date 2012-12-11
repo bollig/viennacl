715d40f4bc8074a95a6fa86d0922114a84f3d9c5
Philippe Tillet <Phil.Tillet@gmail.com>
2012-12-12 01:11:12 +0800

    Removed name arguments bind dummy_vector to viennacl::vector changed custom_operation::add() to accept  a dummy vectors expression

--- viennacl/generator_fromscratch/dummy_types.hpp
+++ viennacl/generator_fromscratch/dummy_types.hpp
@@ -1,31 +1,241 @@
 #ifndef VIENNACL_GENERATOR_DUMMY_TYPES_HPP
 #define VIENNACL_GENERATOR_DUMMY_TYPES_HPP
 
+#include "viennacl/generator_fromscratch/forwards.h"
+#include "viennacl/meta/enable_if.hpp"
 namespace viennacl{
 
 namespace generator{
 
-class dummy_vector{
-
-};
-
-class dummy_scalar{
-
-};
-
-class dummy_matrix{
-
-};
-
 template<class LHS, class OP, class RHS>
 class compile_time_beast{
+public:
+    typedef LHS Lhs;
+    typedef OP Op;
+    typedef RHS Rhs;
+    LHS const & lhs() const{return lhs_;}
+    RHS const & rhs() const{return rhs_;}
+protected:
     compile_time_beast(LHS const & lhs, RHS const & rhs) : lhs_(lhs), rhs_(rhs){ }
-    LHS const & lhs(){return lhs_;}
-    RHS const & rhs(){return rhs_;}
 private:
     LHS const & lhs_;
     RHS const & rhs_;
 };
+
+template<class LHS, class OP, class RHS>
+class vector_expression_wrapper : public compile_time_beast<LHS,OP,RHS>{
+public: vector_expression_wrapper(LHS const & lhs, RHS const & rhs) : compile_time_beast<LHS,OP,RHS>(lhs,rhs){ }
+};
+template<class LHS, class OP, class RHS>
+class scalar_expression_wrapper : public compile_time_beast<LHS,OP,RHS>{
+public: scalar_expression_wrapper(LHS const & lhs, RHS const & rhs) : compile_time_beast<LHS,OP,RHS>(lhs,rhs){ }
+};
+template<class LHS, class OP, class RHS>
+class matrix_expression_wrapper : public compile_time_beast<LHS,OP,RHS>{
+public: matrix_expression_wrapper(LHS const & lhs, RHS const & rhs) : compile_time_beast<LHS,OP,RHS>(lhs,rhs){ }
+};
+
+template<typename ScalarType, unsigned int Alignment=1>
+class dummy_vector{
+    typedef dummy_vector self_type;
+    typedef viennacl::vector<ScalarType,Alignment> vcl_vec_t;
+    vcl_vec_t const & vec_;
+public:
+
+    dummy_vector(vcl_vec_t const & vec): vec_(vec){ }
+
+    vcl_vec_t const & vec() const{ return vec_; }
+
+    template<typename RHS_TYPE>
+    vector_expression_wrapper<self_type, assign_type, RHS_TYPE >
+    operator= ( RHS_TYPE const & rhs ){
+      return vector_expression_wrapper<self_type,assign_type,RHS_TYPE >(*this,rhs);
+    }
+
+    template<typename RHS_TYPE>
+    vector_expression_wrapper<self_type, inplace_scal_mul_type, RHS_TYPE >
+    operator*= ( RHS_TYPE const & rhs ){
+      return vector_expression_wrapper<self_type,inplace_scal_mul_type,RHS_TYPE >(*this,rhs);
+    }
+
+    template<typename RHS_TYPE>
+    vector_expression_wrapper<self_type, inplace_scal_div_type, RHS_TYPE >
+    operator/= ( RHS_TYPE const & rhs ){
+      return vector_expression_wrapper<self_type,inplace_scal_div_type,RHS_TYPE >(*this,rhs);
+    }
+
+    template<typename RHS_TYPE>
+    vector_expression_wrapper<self_type, inplace_add_type, RHS_TYPE >
+    operator+= ( RHS_TYPE const & rhs ){
+      return vector_expression_wrapper<self_type,inplace_add_type,RHS_TYPE >(*this,rhs);
+    }
+
+    template<typename RHS_TYPE>
+    vector_expression_wrapper<self_type, inplace_sub_type, RHS_TYPE >
+    operator-= ( RHS_TYPE const & rhs ){
+      return vector_expression_wrapper<self_type,inplace_sub_type,RHS_TYPE >(*this,rhs);
+    }
+};
+
+class dummy_scalar{
+    typedef dummy_scalar self_type;
+public:
+    template<typename RHS_TYPE>
+    scalar_expression_wrapper<self_type, assign_type, RHS_TYPE >
+    operator= ( RHS_TYPE const & rhs ){
+      return scalar_expression_wrapper<self_type,assign_type,RHS_TYPE >(*this,rhs);
+    }
+
+    template<typename RHS_TYPE>
+    scalar_expression_wrapper<self_type, inplace_scal_mul_type, RHS_TYPE >
+    operator*= ( RHS_TYPE const & rhs ){
+      return scalar_expression_wrapper<self_type,inplace_scal_mul_type,RHS_TYPE >(*this,rhs);
+    }
+
+    template<typename RHS_TYPE>
+    scalar_expression_wrapper<self_type, inplace_scal_div_type, RHS_TYPE >
+    operator/= ( RHS_TYPE const & rhs ){
+      return scalar_expression_wrapper<self_type,inplace_scal_div_type,RHS_TYPE >(*this,rhs);
+    }
+
+    template<typename RHS_TYPE>
+    scalar_expression_wrapper<self_type, inplace_add_type, RHS_TYPE >
+    operator+= ( RHS_TYPE const & rhs ){
+      return scalar_expression_wrapper<self_type,inplace_add_type,RHS_TYPE >(*this,rhs);
+    }
+
+    template<typename RHS_TYPE>
+    scalar_expression_wrapper<self_type, inplace_sub_type, RHS_TYPE >
+    operator-= ( RHS_TYPE const & rhs ){
+      return scalar_expression_wrapper<self_type,inplace_sub_type,RHS_TYPE >(*this,rhs);
+    }
+};
+
+class dummy_matrix{
+    typedef dummy_matrix self_type;
+public:
+    template<typename RHS_TYPE>
+    matrix_expression_wrapper<self_type, assign_type, RHS_TYPE >
+    operator= ( RHS_TYPE const & rhs ){
+      return matrix_expression_wrapper<self_type,assign_type,RHS_TYPE >(*this,rhs);
+    }
+
+    template<typename RHS_TYPE>
+    matrix_expression_wrapper<self_type, inplace_scal_mul_type, RHS_TYPE >
+    operator*= ( RHS_TYPE const & rhs ){
+      return matrix_expression_wrapper<self_type,inplace_scal_mul_type,RHS_TYPE >(*this,rhs);
+    }
+
+    template<typename RHS_TYPE>
+    matrix_expression_wrapper<self_type, inplace_scal_div_type, RHS_TYPE >
+    operator/= ( RHS_TYPE const & rhs ){
+      return matrix_expression_wrapper<self_type,inplace_scal_div_type,RHS_TYPE >(*this,rhs);
+    }
+
+    template<typename RHS_TYPE>
+    matrix_expression_wrapper<self_type, inplace_add_type, RHS_TYPE >
+    operator+= ( RHS_TYPE const & rhs ){
+      return matrix_expression_wrapper<self_type,inplace_add_type,RHS_TYPE >(*this,rhs);
+    }
+
+    template<typename RHS_TYPE>
+    matrix_expression_wrapper<self_type, inplace_sub_type, RHS_TYPE >
+    operator-= ( RHS_TYPE const & rhs ){
+      return matrix_expression_wrapper<self_type,inplace_sub_type,RHS_TYPE >(*this,rhs);
+    }
+};
+
+template<class T>
+struct is_vector_expression_t{ enum { value = 0 }; };
+template<typename ScalarType, unsigned int Alignment>
+struct is_vector_expression_t<dummy_vector<ScalarType,Alignment> >{ enum { value = 1}; };
+template<class LHS, class OP, class RHS>
+struct is_vector_expression_t<vector_expression_wrapper<LHS,OP,RHS> >{ enum { value = 1}; };
+
+template<class T>
+struct is_scalar_expression_t{ enum { value = 0 }; };
+template<>
+struct is_scalar_expression_t<dummy_scalar>{ enum { value = 1}; };
+template<class LHS, class OP, class RHS>
+struct is_scalar_expression_t<scalar_expression_wrapper<LHS,OP,RHS> >{ enum { value = 1}; };
+
+template<class T>
+struct is_matrix_expression_t{ enum { value = 0 }; };
+template<>
+struct is_matrix_expression_t<dummy_matrix>{ enum { value = 1}; };
+template<class LHS, class OP, class RHS>
+struct is_matrix_expression_t<matrix_expression_wrapper<LHS,OP,RHS> >{ enum { value = 1}; };
+
+template<class LHS, class OP, class RHS, bool create_vector, bool create_scalar, bool create_matrix>
+struct convert_to_expr;
+template<class LHS, class OP, class RHS>
+struct convert_to_expr<LHS,OP,RHS,true,false,false>{ typedef vector_expression_wrapper<LHS,OP,RHS> type; };
+template<class LHS, class OP, class RHS>
+struct convert_to_expr<LHS,OP,RHS,false,true,false>{ typedef scalar_expression_wrapper<LHS,OP,RHS> type; };
+template<class LHS, class OP, class RHS>
+struct convert_to_expr<LHS,OP,RHS,false,false,true>{ typedef matrix_expression_wrapper<LHS,OP,RHS> type; };
+
+
+//template<class T>
+//unary_minus<T> operator -(T const &)
+//{
+//  return unary_minus<T>();
+//}
+
+template<class LHS, class RHS>
+typename viennacl::enable_if< (is_vector_expression_t<LHS>::value && is_vector_expression_t<RHS>::value)
+                             ||(is_scalar_expression_t<LHS>::value && is_scalar_expression_t<RHS>::value)
+                             ||(is_matrix_expression_t<LHS>::value && is_matrix_expression_t<RHS>::value)
+                            ,typename convert_to_expr<LHS,add_type,RHS
+                                                    ,is_vector_expression_t<LHS>::value
+                                                    ,is_scalar_expression_t<LHS>::value
+                                                    ,is_matrix_expression_t<LHS>::value>::type>::type
+operator+(LHS const & lhs, RHS const & rhs){
+    return typename convert_to_expr<LHS,add_type,RHS, is_vector_expression_t<LHS>::value, is_scalar_expression_t<LHS>::value, is_matrix_expression_t<LHS>::value>::type(lhs,rhs);
+}
+
+template<class LHS, class RHS>
+typename viennacl::enable_if< (is_vector_expression_t<LHS>::value && is_vector_expression_t<RHS>::value)
+                             ||(is_scalar_expression_t<LHS>::value && is_scalar_expression_t<RHS>::value)
+                             ||(is_matrix_expression_t<LHS>::value && is_matrix_expression_t<RHS>::value)
+                            ,typename convert_to_expr<LHS,sub_type,RHS
+                                                    ,is_vector_expression_t<LHS>::value
+                                                    ,is_scalar_expression_t<LHS>::value
+                                                    ,is_matrix_expression_t<LHS>::value>::type>::type
+operator-(LHS const & lhs, RHS const & rhs){
+    return typename convert_to_expr<LHS,sub_type,RHS, is_vector_expression_t<LHS>::value, is_scalar_expression_t<LHS>::value, is_matrix_expression_t<LHS>::value>::type(lhs,rhs);
+}
+
+template<class LHS, class RHS>
+typename viennacl::enable_if< is_scalar_expression_t<LHS>::value || is_scalar_expression_t<RHS>::value
+                            ,typename convert_to_expr<LHS,scal_mul_type,RHS
+                                                    ,is_vector_expression_t<LHS>::value || is_vector_expression_t<RHS>::value
+                                                    ,is_scalar_expression_t<LHS>::value || is_scalar_expression_t<RHS>::value
+                                                    ,is_matrix_expression_t<LHS>::value || is_matrix_expression_t<RHS>::value>::type>::type
+operator*(LHS const & lhs, RHS const & rhs){
+    return typename convert_to_expr<LHS,scal_mul_type,RHS
+            ,is_vector_expression_t<LHS>::value || is_vector_expression_t<RHS>::value
+            ,is_scalar_expression_t<LHS>::value || is_scalar_expression_t<RHS>::value
+            ,is_matrix_expression_t<LHS>::value || is_matrix_expression_t<RHS>::value>::type(lhs,rhs);
+}
+
+template<class LHS, class RHS>
+typename viennacl::enable_if< is_scalar_expression_t<LHS>::value || is_scalar_expression_t<RHS>::value
+                            ,typename convert_to_expr<LHS,scal_div_type,RHS
+                                                    ,is_vector_expression_t<LHS>::value || is_vector_expression_t<RHS>::value
+                                                    ,is_scalar_expression_t<LHS>::value || is_scalar_expression_t<RHS>::value
+                                                    ,is_matrix_expression_t<LHS>::value || is_matrix_expression_t<RHS>::value>::type>::type
+operator/(LHS const & lhs, RHS const & rhs){
+    return typename convert_to_expr<LHS,scal_div_type,RHS
+            ,is_vector_expression_t<LHS>::value || is_vector_expression_t<RHS>::value
+            ,is_scalar_expression_t<LHS>::value || is_scalar_expression_t<RHS>::value
+            ,is_matrix_expression_t<LHS>::value || is_matrix_expression_t<RHS>::value>::type(lhs,rhs);
+}
+
+
+
+
+
 
 
 }
--- viennacl/generator_fromscratch/forwards.h
+++ viennacl/generator_fromscratch/forwards.h
@@ -18,6 +18,13 @@ class inplace_scal_div_type;
 class elementwise_prod_type;
 class elementwise_div_type;
 
+template<class LHS, class OP, class RHS>
+class compile_time_beast;
+template<typename ScalarType, unsigned int Alignment>
+class dummy_vector;
+class dummy_scalar;
+class dummy_matrix;
+
 }
 
 }
