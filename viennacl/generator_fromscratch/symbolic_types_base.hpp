ae2f2db857a4739ec3f0a9332d8f900b5a4d75e1
Philippe Tillet <Phil.Tillet@gmail.com>
2012-12-14 00:48:46 +0800

    added inner product (though still the generated kernel is not okay)

--- viennacl/generator_fromscratch/symbolic_types_base.hpp
+++ viennacl/generator_fromscratch/symbolic_types_base.hpp
@@ -4,6 +4,8 @@
 #include "viennacl/generator_fromscratch/utils.hpp"
 #include "viennacl/generator_fromscratch/forwards.h"
 #include "viennacl/backend/memory.hpp"
+#include <map>
+
 namespace viennacl{
 
     namespace generator{
@@ -29,14 +31,23 @@ namespace viennacl{
             std::string const & scalartype_;
         };
 
-        class infos_base{
+        struct shared_infos_t{
         public:
-            virtual std::string generate() const = 0;
-            virtual std::string name() const = 0;
-            virtual ~infos_base(){ }
+            shared_infos_t(unsigned int id, std::string scalartype) : id_(id), name_("arg"+to_string(id)), scalartype_(scalartype){ }
+            std::string & access_name(){ return access_name_; }
+            std::string const & name() const{ return name_; }
+            unsigned int id() const{ return id_; }
+            std::string const & scalartype() const{ return scalartype_; }
+        private:
+            std::string access_name_;
+            unsigned int id_;
+            std::string name_;
+            std::string scalartype_;
         };
 
-        class op_infos_base : public infos_base{
+
+
+        class op_infos_base{
         public:
             std::string generate() const{ return expr_; }
             std::string name() const { return name_; }
@@ -49,16 +60,27 @@ namespace viennacl{
             bool is_assignment_;
         };
 
+
         class binary_tree_infos_base{
         public:
-            infos_base & lhs(){ return *lhs_; }
-            infos_base & rhs(){ return *rhs_; }
+            infos_base & lhs() const{ return *lhs_; }
+            infos_base & rhs() const{ return *rhs_; }
+
 
         protected:
             binary_tree_infos_base(infos_base * lhs, infos_base * rhs) : lhs_(lhs), rhs_(rhs){        }
             viennacl::tools::shared_ptr<infos_base> lhs_;
             viennacl::tools::shared_ptr<infos_base> rhs_;
         };
+
+        class infos_base{
+        public:
+            virtual std::string generate() const = 0;
+            virtual std::string name() const = 0;
+            virtual ~infos_base(){ }
+        };
+
+
 
         class unary_tree_infos_base{
         public:
@@ -100,141 +122,130 @@ namespace viennacl{
         };
 
         class kernel_argument : public infos_base{
-        private:
-            virtual viennacl::backend::mem_handle handle() const = 0;
         public:
-            void access_name(std::string const & new_name) { *access_name_ = new_name; }
+            virtual viennacl::backend::mem_handle const & handle() const = 0;
+            kernel_argument( ) { }
+            void access_name(std::string const & new_name) { infos_->access_name() = new_name; }
             virtual ~kernel_argument(){ }
-            virtual std::string generate() const { return *access_name_; }
-            virtual std::string name() const { return name_; }
-            std::string const & scalartype() const { return scalartype_; }
-            kernel_argument( std::string const & name, std::string const & scalartype) :name_(name), scalartype_(scalartype){ }
+            virtual std::string generate() const { return infos_->access_name(); }
+            virtual std::string name() const { return infos_->name(); }
+            std::string const & scalartype() const { return infos_->scalartype(); }
             virtual std::string kernel_arguments() const = 0;
-            bool operator<(kernel_argument const & other) const { return handle() < other.handle(); }
         protected:
-            std::string * access_name_;
-            std::string name_;
-            std::string scalartype_;
+            shared_infos_t* infos_;
+
         };
 
-        class cpu_scal_infos_base : public kernel_argument{
+        class user_kernel_argument : public kernel_argument{ };
+
+        class temporary_kernel_argument : public kernel_argument{ };
+
+        class cpu_scal_infos_base : public user_kernel_argument{
         protected:
-            cpu_scal_infos_base(std::string const & name, std::string const & scalartype) : kernel_argument(name, scalartype){ }
+            cpu_scal_infos_base() { }
         public:
             std::string kernel_arguments() const{
-                return scalartype_ + " " + name_;
+                return scalartype() + " " + name();
             }
         };
 
-        class gpu_scal_infos_base : public kernel_argument{
+        class gpu_scal_infos_base : public user_kernel_argument{
         protected:
-            gpu_scal_infos_base(std::string const & name, std::string const & scalartype) : kernel_argument(name, scalartype){ }
+            gpu_scal_infos_base() { }
         public:
             std::string kernel_arguments() const{
-                return "__global " + scalartype_ + "*"  + " " + name_;
+                return "__global " + scalartype() + "*"  + " " + name();
             }
         };
 
-        class inprod_infos_base : public binary_tree_infos_base, public kernel_argument{
+        class inprod_infos_base : public binary_tree_infos_base,public temporary_kernel_argument{
         public:
             enum step_t{compute,reduce};
             step_t step(){ return *step_; }
             void step(step_t s){ *step_ = s; }
             local_memory make_local_memory(unsigned int size){
-                return local_memory(name_,size,scalartype_);
+                return local_memory(name(),size,scalartype());
             }
 
         protected:
-            inprod_infos_base(
-                              infos_base * lhs, infos_base * rhs,step_t * step
-                              ,std::string const & scalartype): binary_tree_infos_base(lhs,rhs)
-                                               ,kernel_argument(lhs->name()+"ip"+rhs->name(),scalartype)
-                                              ,step_(step){ }
+            inprod_infos_base(infos_base * lhs
+                              , infos_base * rhs
+                              ,step_t * step): binary_tree_infos_base(lhs,rhs),step_(step){ }
 
 
         private:
             viennacl::tools::shared_ptr<step_t> step_;
         };
 
-        class vec_infos_base : public kernel_argument{
+        class vec_infos_base : public user_kernel_argument{
         public:
-            std::string const & size() const{ return size_; }
-            std::string const & internal_size() const{ return internal_size_;}
-            std::string const & start() const{ return start_;}
-            std::string const & inc() const{ return inc_;}
+            std::string  size() const{ return name() + "_size_"; }
+            std::string  internal_size() const{ return name() + "_internal_size_";}
+            std::string  start() const{ return name() + "_start";}
+            std::string  inc() const{ return name() + "_inc";}
             std::string kernel_arguments() const
             {
-              return " __global " + scalartype_ + "*"  + " " + name_
-                  + ", unsigned int " + size_
-                  + ", unsigned int " + internal_size_+ "\n" ;
+              return " __global " + scalartype() + "*"  + " " + name()
+                  + ", unsigned int " + size()
+                  + ", unsigned int " + internal_size()+ "\n" ;
             }
             virtual ~vec_infos_base(){ }
         protected:
-            vec_infos_base(std::string const & name, std::string const & scalartype) :
-                                                            kernel_argument(name,
-                                                                            scalartype)
-                                                          ,size_(name_+"_size")
-                                                          ,internal_size_(""){ }
-        private:
-            std::string size_;
-            std::string internal_size_;
-            std::string start_;
-            std::string inc_;
+            vec_infos_base() { }
         };
 
 
-        class mat_infos_base : public kernel_argument{
+        class mat_infos_base : public user_kernel_argument{
         public:
-            std::string const & size1() const{ return size1_; }
-            std::string const & size2() const{ return size2_; }
-            std::string const & internal_size1() const{ return internal_size1_; }
-            std::string const & internal_size2() const{ return internal_size2_; }
-            std::string const & row_inc() const{ return row_inc_; }
-            std::string const & col_inc() const{ return col_inc_;}
-            std::string const & row_start() const{ return row_start_;}
-            std::string const & col_start() const{ return col_start_;}
+            std::string  size1() const{ return name() +"size1_"; }
+            std::string  size2() const{ return name() +"size2_"; }
+            std::string  internal_size1() const{ return name() +"internal_size1_"; }
+            std::string  internal_size2() const{ return name() +"internal_size2_"; }
+            std::string  row_inc() const{ return name() +"row_inc_"; }
+            std::string  col_inc() const{ return name() +"col_inc_";}
+            std::string  row_start() const{ return name() +"row_start_";}
+            std::string  col_start() const{ return name() +"col_start_";}
             std::string kernel_arguments() const{
-                return " __global " + scalartype_ + "*"  + " " + name_
-                      + ", unsigned int " + row_start_
-                      + ", unsigned int " + col_start_
-                      + ", unsigned int " + row_inc_
-                      + ", unsigned int " + col_inc_
-                      + ", unsigned int " + size1_
-                      + ", unsigned int " + size2_
-                      + ", unsigned int " + internal_size1_
-                      + ", unsigned int " + internal_size2_
+                return " __global " + scalartype() + "*"  + " " + name()
+                        + ", unsigned int " + row_start()
+                        + ", unsigned int " + col_start()
+                        + ", unsigned int " + row_inc()
+                        + ", unsigned int " + col_inc()
+                        + ", unsigned int " + size1()
+                        + ", unsigned int " + size2()
+                        + ", unsigned int " + internal_size1()
+                        + ", unsigned int " + internal_size2()
                       + "\n";
             }
             bool const is_rowmajor() const { return is_rowmajor_; }
             bool const is_transposed() const { return is_transposed_; }
             virtual ~mat_infos_base() { }
         protected:
-            mat_infos_base(std::string const & name
-                           ,std::string const & scalartype
-                           ,bool is_rowmajor
-                           ,bool is_transposed) : kernel_argument(name,scalartype)
-                                                  ,size1_(name_ + "_size1")
-                                                  ,size2_(name_ + "size2")
-                                                  ,internal_size1_(size1_ + "_internal")
-                                                  ,internal_size2_(size2_ + "_internal")
-                                                  ,row_inc_(name_ + "_row_inc")
-                                                  ,col_inc_(name_+"_col_inc")
-                                                  ,row_start_(name_+"_row_start")
-                                                  ,col_start_(name_+"_col_start")
-                                                  ,is_rowmajor_(is_rowmajor)
+            mat_infos_base(bool is_rowmajor
+                           ,bool is_transposed) : is_rowmajor_(is_rowmajor)
                                                   ,is_transposed_(is_transposed){ }
         private:
-            std::string size1_;
-            std::string size2_;
-            std::string internal_size1_;
-            std::string internal_size2_;
-            std::string row_inc_;
-            std::string col_inc_;
-            std::string row_start_;
-            std::string col_start_;
             bool is_rowmajor_;
             bool is_transposed_;
         };
+
+        bool operator<(infos_base const & first, infos_base const & other){
+            if(binary_tree_infos_base const * t = dynamic_cast<binary_tree_infos_base const *>(&first)){
+                return t->lhs() < other && t->rhs() < other;
+            }
+            else if(binary_tree_infos_base const * p= dynamic_cast<binary_tree_infos_base const *>(&other)){
+                return first < p->lhs() && first < p->rhs();
+            }
+            else if(user_kernel_argument const * t = dynamic_cast<user_kernel_argument const *>(&first)){
+                  if(user_kernel_argument const * p = dynamic_cast<user_kernel_argument const*>(&other)){
+                     return t->handle() < p->handle();
+                  }
+            }
+            return false;
+       }
+
+        typedef std::map<viennacl::backend::mem_handle, shared_infos_t> shared_infos_map_t;
+        typedef std::map<infos_base*,viennacl::backend::mem_handle,deref_less> temporaries_map_t;
 
 
     }
